f6a7c6129ad094b830bdd5c76d72316386459280
visualstudio <visualstudio@230-PC13>
2019-02-07 11:55:03 -0500

    Array Changes

--- C0745113/js/main.js
+++ C0745113/js/main.js
@@ -1,1 +1,16 @@
-﻿// Your code here!
+﻿// The Function returns the product of a and b
+function myFunction(a , b){
+ 
+    return a + b;
+}
+
+function arrays() {
+
+    var pens;
+    pens = ["Red", "Blue", "Green","Gray","Maroon","Orange","Black"];
+    console.log(pens);
+    for (i = 0; i < pens.length; i++) {
+        alert(pens[i]);
+    }
+    alert("length of the array is : " +pens.length);
+}
